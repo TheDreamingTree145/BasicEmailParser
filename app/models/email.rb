ad030653f6495ade7be49f5543edf64b3c438d08
dustin <dandersonsoftware@outlook.com>
2018-01-13 15:44:03 -0500

    change way attributes were added to email model

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,14 +2,25 @@ class Email < ApplicationRecord
 
   def self.new_from_email(email)
     new_email = Email.new
-    new_email.sent = email.match(/Date: (.*)/)[1]
-    new_email.receiver = email.match(/Delivered-To: (.*)/)[1]
-    new_email.sender = email.match(/From:\s(.*)<(.*)>/)[2]
-    if email.match(/Cc:/)
-      new_email.cc = email.match(/Cc:\s\w*\s\w*\s(.*)/)[1].gsub(/[<>]/, "")
+    new_email.attributes.keys.each do |key|
+      case key
+      when 'sent'
+        new_email[key] = email.match(/Date: (.*)/)[1]
+        break
+      when 'receiver'
+        new_email[key] = email.match(/Delivered-To: (.*)/)[1]
+      when 'sender'
+        new_email[key] = email.match(/From:\s(.*)<(.*)>/)[2]
+      when 'cc'
+        if email.match(/Cc:/)
+          new_email[key] = email.match(/Cc:\s\w*\s\w*\s(.*)/)[1].gsub(/[<>]/, "")
+        end
+      when 'subject'
+        new_email[key] = email.match(/Subject: (.*)/)[1]
+      when 'content'
+        new_email[key] = email.match(/Content-Type: text\/plain; charset="UTF-8"\s(.*?)\--\b/m)[1]
+      end
     end
-    new_email.subject = email.match(/Subject: (.*)/)[1]
-    new_email.content = email.match(/Content-Type: text\/plain; charset="UTF-8"\s(.*?)\--\b/m)[1]
     new_email.save
     new_email
   end
